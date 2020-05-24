48f79cb24e4e894fd7aa7ba7c5dfa3dcef84c95f
woom <saewoomk88@gmail.com>
2020-05-24 18:15:40 +0900

    Initial commit

---
+++ src/main/java/org/zerock/sample/SamleHotel.java
@@ -0,0 +1,16 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@Getter
+@RequiredArgsConstructor
+public class SamleHotel {
+
+	private final Chef chef;
+	
+}
